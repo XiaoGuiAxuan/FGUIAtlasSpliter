2b2f7697a89ad5526b447b66c4d8006c0b9f3d0f
XiaoGuiAxuan <767726342@qq.com>
2021-07-23 15:58:06 +0800

    首次提交

---
+++ UIPackage/PackageItem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public class PixelHitTestData
+{
+    public int pixelWidth;
+    public float scale;
+    public byte[] pixels;
+    public int pixelsLength;
+    public int pixelsOffset;
+
+    public void Load(ByteBuffer ba)
+    {
+        ba.ReadInt();
+        pixelWidth = ba.ReadInt();
+        scale = 1.0f / ba.ReadByte();
+        pixels = ba.buffer;
+        pixelsLength = ba.ReadInt();
+        pixelsOffset = ba.position;
+        ba.Skip(pixelsLength);
+    }
+}
+/// <summary>
+/// 
+/// </summary>
+public class PackageItem
+{
+    public UIPackage owner;
+
+    public PackageItemType type;
+    public ObjectType objectType;
+
+    public string id;
+    public string name;
+    public int width;
+    public int height;
+    public string path;
+    public string file;
+    public bool exported;
+    //public NTexture texture;
+    public ByteBuffer rawData;
+    public string[] branches;
+    public string[] highResolution;
+
+    //image
+    public Rect? scale9Grid;
+    public bool scaleByTile;
+    public int tileGridIndice;
+    public PixelHitTestData pixelHitTestData;
+
+
+    public PackageItem getBranch()
+    {
+        if (branches != null && owner._branchIndex != -1)
+        {
+            string itemId = branches[owner._branchIndex];
+            if (itemId != null)
+                return owner.GetItem(itemId);
+        }
+
+        return this;
+    }
+
+    public PackageItem getHighResolution()
+    {
+        var contentScaleLevel = 0;
+        if (highResolution != null && contentScaleLevel > 0)
+        {
+            int i = contentScaleLevel - 1;
+            if (i >= highResolution.Length)
+                i = highResolution.Length - 1;
+            string itemId = highResolution[i];
+            if (itemId != null)
+                return owner.GetItem(itemId);
+        }
+
+        return this;
+    }
+}
---
+++ UIPackageTool.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class UIPackageTool
+{
+    /// <summary>
+    /// 切割图集
+    /// </summary>
+    /// <param name="byteFile">二进制路径</param>
+    /// <param name="exportDir">导出的目录</param>
+    /// <param name="isOverrideExists">是否覆盖已经存在的散图</param>
+    public static void SplitAtlas(string byteFile, string exportDir,bool isOverrideExists = true)
+    {
+        // 创建导出文件夹
+        string outPath = $"{exportDir}/{Path.GetFileNameWithoutExtension(byteFile)}/";
+        if (!Directory.Exists(outPath))
+        {
+            Directory.CreateDirectory(outPath);
+        }
+
+        // bytes
+        var source = File.ReadAllBytes(byteFile);
+        var dir = Path.GetDirectoryName(byteFile);
+        ByteBuffer buffer = new ByteBuffer(source);
+
+        // UIPackage
+        UIPackage pkg = new UIPackage();
+        var mainAssetName = Path.GetFileNameWithoutExtension(byteFile);
+        pkg.LoadPackage(buffer, mainAssetName);
+        var sprites = pkg.sprites;
+
+        // Atlas Map
+        Dictionary<string, Bitmap> atlasMap = new Dictionary<string, Bitmap>();
+        foreach (var item in pkg.GetItems())
+        {
+            if (item.type == PackageItemType.Atlas)
+            {
+                //Console.WriteLine($"加载图集：{item.file}");
+                var bitmap = Image.FromFile($"{dir}/{item.file.Replace("_fui", string.Empty)}") as Bitmap;
+                atlasMap.Add(item.file, bitmap);
+            }
+        }
+
+
+        foreach (var item in sprites)
+        {
+            var key = item.Key;
+            var atlasSprite = item.Value;
+
+            // PackageItem
+            var pItem = pkg.GetItem(key);
+            if (pItem == null) continue;
+            var name = pItem.name;
+            var rect = atlasSprite.rect;
+            var rotated = atlasSprite.rotated;
+            var originalSize = atlasSprite.originalSize;
+            var offset = atlasSprite.offset;
+            var atlas = atlasMap[atlasSprite.atlas.file];
+
+            var newTexPath = $"{outPath}/{name}.png";
+
+            if (!isOverrideExists && File.Exists(newTexPath))
+            {
+                continue;
+            }
+
+            var width = (int)rect.width;
+            var height = (int)rect.height;
+            Bitmap tex = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            var x = (int)rect.x;
+            var y = (int)rect.y;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    var _x = x + i;
+                    var _y = y + j;
+                    var col = atlas.GetPixel(_x, _y);
+                    tex.SetPixel(i, j, col);
+                }
+            }
+
+            if (rotated)
+            {
+                tex.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            }
+
+           
+            tex.Save(newTexPath);
+
+        }
+    }
+
+
+}
